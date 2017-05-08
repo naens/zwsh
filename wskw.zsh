9a3cc97e1064c4956ae207e8811b3a8e3f5f865b
naens <naens@gmx.com>
2017-05-08 22:12:10 +0200

    fix function names

--- wskw.zsh
+++ wskw.zsh
@@ -3,8 +3,8 @@ wsdialog_kwdial_msg="Write to file: "
 wsdialog_kwdial_modes[1]=badfn
 wsdialog_kwdial_modes[2]=fexists
 wsdialog_kwdial_modes[3]=ewrite
-wsdialog_kwdial_accept=kw2-accept
-wsdialog_kwdial_restore=kw2-restore
+wsdialog_kwdial_accept=kw-accept
+wsdialog_kwdial_restore=kw-restore
 
 # badfn
 wsdialog_kwdial_badfn_msg="Bad file name."
@@ -12,8 +12,8 @@ wsdialog_kwdial_badfn_msg="Bad file name."
 # fexists (yes=overwrite, no=edit)
 wsdialog_kwdial_fexists_msg="#That file already exists.# Overwrite (Y/N)?"
 declare -A wsdialog_kwdial_fexists_funcs
-wsdialog_kwdial_fexists_funcs[y]=kw2-fexists-yes
-wsdialog_kwdial_fexists_funcs[n]=kw2-fexists-no
+wsdialog_kwdial_fexists_funcs[y]=kw-fexists-yes
+wsdialog_kwdial_fexists_funcs[n]=kw-fexists-no
 
 # ewrite
 make-ewrite-msg() {
@@ -23,13 +23,13 @@ make-ewrite-msg() {
 
 wsdialog-add kwdial
 
-kw2-accept() {
+kw-accept() {
     if [[ -z "$wsdialog_text" || ${#wsdialog_text} -eq 0 ]]; then
         wsdialog_l4mode=badfn
     elif [[ -e "$wsdialog_text" ]]; then
         wsdialog_l4mode=fexists
     else
-        if kw2-save "$wsdialog_text" "$wsblock_text"; then
+        if kw-save "$wsdialog_text" "$wsblock_text"; then
             unset wsdialog_l4mode
         else
             make-ewrite-msg
@@ -38,11 +38,11 @@ kw2-accept() {
     fi
 }
 
-kw2-restore() {
+kw-restore() {
 }
 
-kw2-fexists-yes() {
-    if kw2-save "$wsdialog_text" "$wsblock_text"; then
+kw-fexists-yes() {
+    if kw-save "$wsdialog_text" "$wsblock_text"; then
         wsdialog_l4mode="<accept>"
     else
         make-ewrite-msg
@@ -50,12 +50,12 @@ kw2-fexists-yes() {
     fi
 }
 
-kw2-fexists-no() {
+kw-fexists-no() {
     unset wsdialog_l4mode
 }
 
 # file in first argument, text in second argument
-kw2-save() {
+kw-save() {
     $ws_echo "$2" 2>&- > "$1"
     return $?
 }
