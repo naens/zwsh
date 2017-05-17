0e5f77eed1d669e30de120086473f4f983af4da5
naens <naens@gmx.com>
2017-05-17 15:39:24 +0200

    fix main pre-draw: cursor position after ^I completion

--- wskeys.zsh
+++ wskeys.zsh
@@ -296,6 +296,7 @@ zle-line-pre-redraw () {
 
 main-pre-redraw() {
     ws_text="$BUFFER" # TODO: on tab expand: redefine ws_text
+    ws_curs=$CURSOR    
 #    ws-updfn # temporary
 #    echo MAIN buffer="$BUFFER" > $debugfile
 }
