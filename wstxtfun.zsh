c8bb576a3f844d87d0aa573f1dd963eb44c868b7
naens <naens@gmx.com>
2017-05-18 22:32:21 +0200

    add text navigation function module

---
+++ wstxtfun.zsh
@@ -0,0 +1,46 @@
+wstxtfun-prev-word() {
+    local pos=$1
+    local text="$2"
+
+    # skip until next word
+    local i=$pos
+    while [[ ! "$text[i]" =~ [[:alnum:]] && $i -ge 1 ]]; do
+        i=$((i-1))
+    done
+
+    # skip word or end of text
+    while [[ "$text[i]" =~ [[:alnum:]] && $i -ge 1 ]]; do
+        i=$((i-1))
+    done
+    echo $i
+}
+
+wstxtfun-next-word() {
+    local pos=$1
+    local text="$2"
+    local text_end=${#text}
+
+    # skip word or end of text
+    local i=$((pos+1))
+    while [[ "$text[i]" =~ [[:alnum:]] && $i -le $text_end ]]; do
+        i=$((i+1))
+    done
+
+    # skip until next word
+    while [[ ! "$text[i]" =~ [[:alnum:]] && $i -le $text_end ]]; do
+        i=$((i+1))
+    done
+    echo $((i-1))
+}
+
+wstxtfun-end-word() {
+    local pos=$1
+    local text="$2"
+    local text_end=${#text}
+
+    local i=$((pos+1))
+    while [[ "$text[i]" =~ [[:alnum:]] && $i -le $text_end ]]; do
+        i=$((i+1))
+    done
+    echo $((i-1))
+}
