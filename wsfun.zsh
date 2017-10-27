7e7c370b5ee8138e19cbde74e45acde26fa3188d
naens <naens@gmx.com>
2017-10-28 00:22:51 +0200

    updatoes

--- wsfun.zsh
+++ wsfun.zsh
@@ -167,7 +167,7 @@ ws-printvar() {
 # debug
 ws-debug() {
     local debug_string="$@"
-#    ws_debugfile=/dev/pts/3
+#    ws_debugfile=/dev/pts/2
     ws_debugfile=/dev/null
     echo "$debug_string" > "$ws_debugfile"
 }
