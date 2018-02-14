b43548e8af51987b40090f85e55c5f1a33562861
naens <naens@gmx.com>
2018-02-15 00:07:34 +0100

    zwshinst.sh: improve installation (fix a little bit)

--- zwshinst.sh
+++ zwshinst.sh
@@ -1,15 +1,30 @@
 #!/bin/sh
 set -e
+
+if [ $UID != 0 ]; then
+    echo "Please run this script with sudo."
+    exit 1
+fi
+
+if [ -z "$SUDO_USER" ]; then
+	echo "Error: SUDO_USER not set."
+	exit 1
+fi
+
+homedir=$(getent passwd $SUDO_USER | cut -d: -f6)
 basedir=$(dirname $0)
 tmp="$basedir/*.zsh"
 files=$(echo $tmp)
 mkdir -pv /opt/zwsh
 cp -v $files /opt/zwsh
-echo "set ZWSHDIR to /opt/zwsh"
 
-if [[ -f ~/.zshrc ]]; then
-    cp .zshrc .zshrc.bak
+if [ -f "$homedir/.zshrc" ]; then
+    cp "$homedir/.zshrc" "$homedir/.zshrc.bak"
 fi
-cp .zshrc ~
-export ZWSHDIR=/opt/zwsh
+cp .zshrc "$homedir"
 
+for f in "$homedir/".zshrc{,.bak}; do
+	if [ -f "$f" ]; then
+		chown $SUDO_USER "$f"
+	fi
+done
